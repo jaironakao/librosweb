b7a9f659881c9cb104dd50484aa0fe41500c1774
sakretsu <sakretsu@warmachine.(none)>
2015-05-28 12:00:31 -0400

    Anade comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522191105) do
+ActiveRecord::Schema.define(version: 20150527203836) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["person_id"], name: "index_comments_on_person_id"
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
